5f642a2084fcd1ecbc7b62c645f7b36b33ab1d7a
Daniel Dean <dd@danieldean.uk>
2020-11-04 19:38:55 +0000

    Add support for heat mat and fan control.

--- vivarium_ctrl.py
+++ vivarium_ctrl.py
@@ -17,6 +17,7 @@ import sqlite3
 import datetime
 import board
 import constants
+from gpiozero import Energenie
 
 
 def read_sensor(dht_device, num_retries=3):
@@ -34,12 +35,25 @@ def read_sensor(dht_device, num_retries=3):
                 raise error
 
 
+def to_string(value):
+    """ Convert a boolean to on/off as a string.
+    """
+    if value:
+        return "On"
+    else:
+        return "Off"
+
+
 def main():
 
     # Initialise sensor, database connection and cursor.
     dht_device = adafruit_dht.DHT22(board.D4)  # .DHT11(board.D4)
     db = sqlite3.connect('vivarium_ctrl.db')
     c = db.cursor()
+
+    # Initialise devices.
+    heat_mat = Energenie(constants.HEAT_MAT_SOCKET)
+    fan = Energenie(constants.FAN_SOCKET)
 
     # Create the sensor readings table if it does not already exist.
     c.execute('CREATE TABLE IF NOT EXISTS sensor_readings (reading_datetime TEXT, temperature NUMERIC, '
@@ -54,11 +68,30 @@ def main():
 
         # Insert readings (or failure) into the database.
         if humidity is not None and temperature is not None:
-            c.execute('INSERT INTO sensor_readings VALUES (?,?,?,?)',
-                      (datetime.datetime.now(), temperature, humidity, 'Read successful.'))
+
+            # Turn the heater on if temperature is low.
+            if temperature <= constants.LOW_TEMPERATURE and not heat_mat.value:
+                heat_mat.on()
+            elif temperature > constants.LOW_TEMPERATURE and heat_mat.value:
+                heat_mat.off()
+
+            # Turn the fan on if temperature is high.
+            if temperature >= constants.HIGH_TEMPERATURE and not fan.value:
+                fan.on()
+            elif temperature < constants.HIGH_TEMPERATURE and fan.value:
+                fan.off()
+
+            # Write read status and device states to the database.
+            comments = "Read successful. Heat Mat: " + to_string(heat_mat.value) + ", Fan: " + to_string(fan.value)
+
         else:
-            c.execute('INSERT INTO sensor_readings VALUES (?,?,?,?)',
-                      (datetime.datetime.now(), temperature, humidity, 'Read failed.'))
+
+            # Nothing else to do without successful sensor readings. Perhaps fallback code needed for failure.
+            comments = "Read failed. Heat Mat: " + to_string(heat_mat.value) + ", Fan: " + to_string(fan.value)
+
+        # Insert and commit.
+        c.execute('INSERT INTO sensor_readings VALUES (?,?,?,?)',
+                  (datetime.datetime.now(), temperature, humidity, comments))
         db.commit()
 
         # Exit closing database.
