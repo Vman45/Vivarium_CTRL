e173a31433210a43eb7aa7fdff1b3d64fe2fc207
Daniel Dean <dd@danieldean.uk>
2020-11-06 20:35:38 +0000

    Add device state table.

--- vivarium_ctrl.py
+++ vivarium_ctrl.py
@@ -47,6 +47,16 @@ def main():
               'humidity NUMERIC, comments TEXT)')
     db.commit()
 
+    # Create a device states table and initialise all as off.
+    device_states = [('heat-mat', 0),
+                     ('pump', 0),
+                     ('fan', 0),
+                     ('light', 0)]
+    c.execute('DROP TABLE IF EXISTS device_states')
+    c.execute('CREATE TABLE device_states (device TEXT, state NUMERIC)')
+    c.executemany('INSERT INTO device_states VALUES (?,?)', device_states)
+    db.commit()
+
     # Continue running until interrupted.
     while True:
 
@@ -56,14 +66,22 @@ def main():
         # Turn the heater on if temperature is low.
         if temperature <= constants.LOW_TEMPERATURE and not heat_mat.value:
             heat_mat.on()
+            c.execute("UPDATE device_states SET state=1 WHERE device='heat-mat'")
+            db.commit()
         elif temperature > constants.LOW_TEMPERATURE and heat_mat.value:
             heat_mat.off()
+            c.execute("UPDATE device_states SET state=0 WHERE device='heat-mat'")
+            db.commit()
 
         # Turn the fan on if temperature is high.
         if temperature >= constants.HIGH_TEMPERATURE and not fan.value:
             fan.on()
+            c.execute("UPDATE device_states SET state=1 WHERE device='fan'")
+            db.commit()
         elif temperature < constants.HIGH_TEMPERATURE and fan.value:
             fan.off()
+            c.execute("UPDATE device_states SET state=0 WHERE device='fan'")
+            db.commit()
 
         # Write read status and device states to the database.
         comments = "Heat Mat: " + to_string(heat_mat.value) + ", Fan: " + to_string(fan.value)
